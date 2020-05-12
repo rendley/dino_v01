21bf5ef943cec1f4dd04eb2546669f14373c84ec
ivan_grigorev <rauscral@gmail.com>
2020-05-12 21:02:50 +0300

    create model Post and customization admin panel

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """list_display - отображение желаемых полей
+       list_filter - фильтрация по выбранным полям
+       search_fields - поиск по полям
+       prepopulated_fields - генерация slug из title
+       date_hierarchy - отображении навиации дат по ссылкам
+       ordering - сортировка по умолчанию
+       raw_id_fields - поиск автора а не выплывающий список
+       авторов постребуется если много авторов
+    """
+    list_display = ('title', 'slug', 'author', 'publish', 'status',)
+    list_filter = ('status', 'created', 'publish', 'author',)
+    search_fields = ('title', 'body',)
+    prepopulated_fields = {'slug': ('title',)}
+    # raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish',)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель постов"""
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
+    title = models.CharField('Заголовок', max_length=250, db_index=True)
+    slug = models.SlugField('url', max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE, related_name='posts')
+    body = models.TextField('Содержание', blank=True)
+    publish = models.DateTimeField('Дата публикации', default=timezone.now)
+    created = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated = models.DateTimeField('Дата редактирования', auto_now=True)
+    status = models.CharField('Статус', max_length=20, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
