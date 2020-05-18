9fcd47a385826d2a1853662d193a5226f17348f0
ivan_grigorev <rauscral@gmail.com>
2020-05-18 03:56:29 +0300

    forms Mixin for use widgets bootstrap and form auth + registr

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -1,33 +1,48 @@
 from django import forms
+from django.contrib.auth.models import User
+
 from .models import Comment, Post
 
 
-
-class CreatePostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'body',)
-
-    # widgets bootstrap
+class WidgetMixinForm(forms.ModelForm):
+    """Mixin для переопределения отображения
+       форм на 'form-control' bootstrap
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
 
-class EmailPostForm(forms.Form):
+class CreatePostForm(WidgetMixinForm, forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body',)
+
+
+class AuthUserForm(WidgetMixinForm, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password',)
+
+
+class RegisterUserForm(WidgetMixinForm, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username',  'email', 'password',)
+
+
+class CommentForm(WidgetMixinForm, forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+
+class EmailPostForm(WidgetMixinForm, forms.Form):
     name = forms.CharField(max_length=50)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(widget=forms.Textarea)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'
 
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('body',)
