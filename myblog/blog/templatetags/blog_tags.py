9e8ae44d01fde9f9203a469d5460a2806779c0b8
ivan_grigorev <rauscral@gmail.com>
2020-05-28 03:48:44 +0300

    Add own template tags with statistic site

---
+++ myblog/blog/templatetags/blog_tags.py
@@ -0,0 +1,35 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+
+
+register = template.Library()
+
+# Total posts at site
+@register.simple_tag(name='total_post_tag')
+# Create template tag
+# The name it isn't necessary argument
+# if not name in decorator then call name func {% total_post %}
+# after add tag reboot server and {% load %} in templates
+def total_post():
+    return Post.objects.count()
+
+
+# Most commented posts
+@register.simple_tag
+def get_post_most_comments(count=5):
+    """
+    add all comment for post and sort and slicing
+    """
+    return Post.objects.annotate(total_comments=Count('comments'))\
+                                .order_by('-total_comments')[:count]
+
+# Last articles at site
+@register.inclusion_tag('blog/tags/latest_post.html')
+# Create template tag inclusion
+# The argument template name
+# context goes in 'blog/tags/latest_post.html'
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts, }
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -126,12 +126,12 @@ class TagDetailView(DetailView):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, )
     form = CommentForm()
-    # Формирование списка похожих статей.
+    # список похожих статей.
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
                         .order_by('-same_tags', '-publish')[:5]
-    print(similar_posts)
+
     return render(request, 'blog/post_detail.html', {'post': post,
                                                      'form': form,
                                                      'similar_posts': similar_posts, })
